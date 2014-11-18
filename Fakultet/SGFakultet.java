a2459e60b13b38fb6b7966115da4a67d9b6a7e10
maxrevenge <maxrevenge95@gmail.com>
2014-11-18 11:07:01 +0300

    Revert "commit"
    This reverts commit a0fe560c6615cd2010c23a1cb45b73cb7bd6a8fc.

--- Fakultet/SGFakultet.java
+++
@@ -1,9 +0,0 @@
-package Fakultet;
-
-public interface SGFakultet {
-	public void setFakultet(Fakultet fk);
-
-	public void setFakultet(String arg1, String arg2, int arg3);
-
-	public Fakultet getFakultet();
-}
